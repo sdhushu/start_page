98aa6930778c2e1fa394bbfdb83dcb75c6337fb9
vike <Email>
2022-01-30 23:22:39 +0800

    fix(base path)

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path = require("path");
+import path from 'path'
 
 const srcPath = path.resolve(__dirname, 'src');
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'/',
+  base:'./',
   publicDir:'public',
   resolve:{
     alias:[
