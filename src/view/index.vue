4dae95d4828b63ae7857d133a00a0beb7ac6de61
vike <Email>
2022-01-30 23:03:39 +0800

    feat(project init)

---
+++ src/view/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="page">
+    <h1>首页 基础示例</h1>
+  </div>
+</template>
+
+
+<style scoped lang="less">
+.page {
+  width: 100%;
+  height: 100%;
+  background-size: 100% 100%;
+  background-image: url("@/assets/image/bground.jpg");
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path = require("path");
+
+const srcPath = path.resolve(__dirname, 'src');
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base:'/',
+  publicDir:'public',
+  resolve:{
+    alias:[
+      {find:'@',replacement:srcPath}
+    ]
+  }
+})
