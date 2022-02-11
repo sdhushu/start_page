1102f1339c057b59077ee068b125b70d80ce12c1
vike <Email>
2022-02-11 11:08:20 +0800

    fix(Theme less add)

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router";
 import './styles/reset.less'
-import './theme/style.less'
+import './theme/theme.less'
 
 createApp(App)
     .use(router)
