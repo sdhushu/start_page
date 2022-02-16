fe504e47d5c09fea64d23402e912383ae7f5102c
Zorn Wang <>
2022-02-17 01:36:04 +0800

    Search代码逻辑修改

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,65 +3,55 @@ import {defineComponent, ref} from "vue";
 
 export default defineComponent({
   setup() {
+    interface searchEngine {
+      id: number;
+      name: string;
+      description: string;
+      url: string;
+      value: string;
+    }
+
     const searchPrefix = ref("https://www.baidu.com/s");
     const searchParam = ref("");
     const place = ref("百度一下");
+    const current_index = ref(0);
 
-    const active = {
-      百度一下: false,
-      谷歌搜索: false,
-      微软Bing: false,
-      源代码搜索: false,
-      哔哩哔哩搜索: false
+    const searchEngines = [
+      {id: 1, name: '百度', description: '百度一下', url: 'https://www.baidu.com/s', value: 'wd'},
+      {id: 2, name: '谷歌', description: '谷歌搜索', url: 'https://www.google.com.hk/search', value: 'query'},
+      {id: 3, name: '必应', description: '微软Bing', url: 'https://cn.bing.com/search', value: 'q'},
+      {id: 4, name: 'Github', description: '源代码搜索', url: 'https://github.com/search', value: 'q'},
+      {id: 5, name: 'Bilibili', description: '哔哩哔哩搜索', url: 'https://search.bilibili.com/all', value: 'keyword'},
+    ];
+
+    const handleSearchTabClick = (index: number, value: searchEngine) => {
+      current_index.value = index;
+      searchPrefix.value = value.url;
+      searchParam.value = value.value;
+      place.value = value.description;
     };
-
-    const isActive = ref({...active});
-    console.log(isActive)
-
-    function setVal(key: keyof typeof isActive.value, val: boolean) {
-      isActive.value[key] = val
-    }
-
-    const getVal = (event: any) => {
-      searchPrefix.value = event?.target.getAttribute("url");
-      searchParam.value = event.target.value;
-      place.value = event.target.placeholder;
-      isActive.value = {...active};
-      setVal(event.target.placeholder, true);
-      console.log(isActive);
-    }
 
     return {
       searchPrefix,
       searchParam,
       place,
-      isActive,
-      getVal
+      current_index,
+      searchEngines,
+      handleSearchTabClick
     }
   }
 })
 </script>
 <template>
   <div class="search">
-    <div class="search-nav">
-      <input checked hidden type='radio' name="type" id='type-baidu' value="wd" placeholder='百度一下' @click="getVal"
-             url="https://www.baidu.com/s">
-      <label for='type-baidu' class="active" :class="{ selected: isActive.百度一下}"><span>百度</span></label>
-      <input checked hidden type='radio' name="type" id='type-google' value='query' placeholder='谷歌搜索' @click="getVal"
-             url="https://www.google.com.hk/search">
-      <label for='type-google' class="active" :class="{ selected: isActive.谷歌搜索}"><span>谷歌</span></label>
-      <input checked hidden type='radio' name="type" id='type-bing' value='q' placeholder='微软Bing' @click="getVal"
-             url="https://cn.bing.com/search">
-      <label for='type-bing' class="active" :class="{ selected: isActive.微软Bing}"><span>必应</span></label>
-      <input checked hidden type="radio" name="type" id="type-github" value="q" placeholder="源代码搜索" @click="getVal"
-             url="https://github.com/search">
-      <label for='type-github' class="active" :class="{ selected: isActive.源代码搜索}"><span>Github</span></label>
-      <input checked hidden type="radio" name="type" id="type-bilibili" value="keyword" placeholder="哔哩哔哩搜索"
-             @click="getVal" url="https://search.bilibili.com/all">
-      <label for='type-bilibili' class="active" :class="{ selected: isActive.哔哩哔哩搜索}"><span>Bilibili</span></label>
-    </div>
+    <ul class="search-nav">
+      <li v-for="(value,index) in searchEngines" @click="handleSearchTabClick(index,value)" class="active"
+          :class="{ selected: current_index==index}">
+        {{ value.name }}
+      </li>
+    </ul>
     <div class="search-box">
-      <form :action="searchPrefix" method="get" target="_blank">
+      <form class="search-box-items" :action="searchPrefix" method="get" target="_blank">
         <input type="text" class="search-in" :name="searchParam" :placeholder="place">
         <button class="search-btn" type="submit"><img src="../assets/image/search-normal.svg"></button>
       </form>
@@ -82,6 +72,7 @@ export default defineComponent({
 
 .search-nav {
   display: flex;
+  flex-direction: row;
   position: relative;
   left: 40px;
   width: auto;
@@ -99,13 +90,6 @@ export default defineComponent({
 }
 
 .active:hover {
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  font-size: 18px;
-  width: 72px;
-  line-height: 32px;
-  color: #ffffff;
   background: rgba(255, 255, 255, 0.3);
   backdrop-filter: blur(200px);
   border-radius: 10px 10px 0px 0px;
@@ -118,6 +102,9 @@ export default defineComponent({
 }
 
 .search-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 600px;
   height: 50px;
   background: rgba(255, 255, 255, 0.3);
@@ -127,22 +114,26 @@ export default defineComponent({
   border-radius: 57px;
 }
 
+.search-box-items {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+}
+
 .search-in {
   outline-style: none;
   border: 0ch;
-  margin: 0 0 0 4%;
-  padding: 16px 0px;
-  width: 80%;
+  margin-left: 16px;
   background: transparent;
   font-size: 16px;
 }
 
 .search-btn {
-  position: absolute;
+  //position: absolute;
   width: 60px;
   height: 32px;
-  right: 8px;
-  top: calc(50% - 32px / 2);
+  margin-right: 8px;
   background: #3D8CFF;
   border-radius: 30px;
   outline-style: none;
